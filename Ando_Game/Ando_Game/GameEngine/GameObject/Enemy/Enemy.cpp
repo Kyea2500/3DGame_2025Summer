766a4a11b6da547877543b66d7a8f263bae2aa43
Kyea2500 <fko2447064@stu.o-hara.ac.jp>
2025-08-06 15:01:16 +0900

    up

--- Ando_Game/Ando_Game/GameEngine/GameObject/Enemy/Enemy.cpp
+++ Ando_Game/Ando_Game/GameEngine/GameObject/Enemy/Enemy.cpp
@@ -7,7 +7,7 @@ namespace
 	// 敵の当たり判定の色
 	constexpr int kEnemyColor = 16; // 敵の当たり判定の色
 	// 敵の移動速度
-	constexpr float kEnemySpeed = 10.0f; // 敵の移動速度
+	constexpr float kEnemySpeed = 5.0f; // 敵の移動速度
 }
 
 Enemy::Enemy() : EnemyHandle(-1),
@@ -61,12 +61,15 @@ void Enemy::Update()
 	}
 	//めためたについてくようになった
 	// とは言えこれでは早すぎるため少しだけ早さを下げる
-	// …っと言いたいところだが、敵をプレイヤーの方向に向く処理を入れるため、一旦後回しにする
-	// 今回は例の通り、プレイヤーの位置に敵の向きを合わせる処理をする
-	// とはいえX軸のみである
-	// そのため、敵の向きはプレイヤーの位置に合わせてX軸のみを向くようにする
-	// プレイヤーの位置＝敵の正面にしたい
-		
+	
+	// 速度の調整完了
+	// 続いては向きの調整に移りたい
+	// プレイヤーの位置を取得
+//	m_playerPos = m_pPlayer->GetTransform()->GetPosition(); // プレイヤーの位置を取得
+
+
+
+
 	MV1SetPosition(EnemyHandle, m_transform->GetPosition()); // 敵の位置を更新 
 }
 
@@ -96,4 +99,3 @@ float Enemy::GetColRadius() const
 {
 	return  kColRadius;
 }
-
